6fb5dbe219ba076dbd9988981c7898631c133858
Pietro-H <pietro.henrique.pereira@escola.pr.gov.br>
2025-11-15 00:09:25 +0000

    aula1

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-const lampada = document.getElementById("lampada");
+const pntsemente = document.getElementById("semente");
+const pntplantinha = document.getElementById("plantinha");
+const pntplanta = document.getElementById("planta");
 
-lampada.addEventListener("mouseover",acender);
-lampada.addEventListener("mouseout",apagar);
+pntsemente.addEventListener("mouseover",crescersemente);
+pntplantinha.addEventListener("mouseover",crescerplantinha);
+pntplanta.addEventListener("mouseout",crescerplanta);
 
-function acender(){
-    lampada.src = "on.png";
-    document.body.style.background = "rgba(255, 217, 2, 1)"
+const btnsemente = document.getElementById("btnsemente")
+const btnplantinha = document.getElementById("btnplantinha")
+const btnplanta = document.getElementById("btnplanta")
+const btnAuto = document.getElementById("btnAuto")
+const btnParar = document.getElementById("btnParar")
+
+let intervalo;
+
+function limpar(){
+    pntsemente.className = 'pnt';
+    pntplantinha.className = 'pnt';
+    pntplanta.className = 'pnt';
 }
-function apagar(){
-    lampada.src = "off.png";
-    document.body.style.background =
+
+function crescersemente(){
+    pntsemente.src = "img1.jpeg";
+}
+function crescerplantinha(){
+    pntplantinha.src ="img2.jpeg";
+}
+function crescerplanta(){
+    pntplanta.src ="img3.jpeg";
+}
+
+function modoAutomatico(){
+    limpar();
+    let estado =0;
+    intervalo = setInterval(()=>{
+        if(estado === 0 ) crescersemente();
+        else if (estado === 1) crescerplantinha();
+        else if (estado === 2) crescerplanta();
+        estado = (estado + 1) % 3;
+    }, 1000)
+}
+
+function parar(){
+    clearInterval(intervalo);
+    limpar();
+}
+
+btnParar.onclick = parar;
+btnAuto.onclick = modoAutomatico;
